3c956b3cf889cce2862e571beeeb7d5434edd40e
h4rl <h4rldev@proton.me>
2025-08-16 15:41:32 +0200

    feat: add power

--- system/nisemono/home/programs/nushell.nix
+++ system/nisemono/home/programs/nushell.nix
@@ -1,10 +1,12 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  nushellScripts = ../nu;
+in {
   programs.nushell = {
     enable = true;
     plugins = with pkgs.nushellPlugins; [
-      net
+      # net
       skim
-      dbus
+      # dbus
       query
       gstat
       semver
@@ -17,7 +19,20 @@
     };
 
     extraConfig = ''
-      source "../nu/oh-my-v2.nu"
+      use ${nushellScripts}/power.nu
+          use ${nushellScripts}/power_git.nu
+          power inject 0 1 {source: git,   color: '#504945'}
+          use ${nushellScripts}/power_kube.nu
+          power inject 1 2 {source: kube,  color: '#504945'} {
+              context: cyan
+          } {
+              reverse: true
+              separator: '@'
+          }
+          use ${nushellScripts}/power_utils.nu
+          power inject 0 1 {source: atuin, color: '#4C4B4A'}
+          power set time null { style: compact }
+      power init
     '';
   };
 }
